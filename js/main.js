3f8bf0581f8d7cd024eee55291177437c800898c
leegutler <135056173+leegutler@users.noreply.github.com>
2023-05-31 16:14:39 +0300

    Day14-Sprint1 - just basic-checking github

---
+++ js/main.js
@@ -0,0 +1,127 @@
+'use strict'
+// Pieces Types
+const MINE = '💣'
+const FLAG = '🚩'
+const FLOOR = '*'
+
+/// MODAL
+
+var gBoard
+var gIsGameOn
+var gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+
+function onInit() {
+    gBoard = buildBoard()
+    renderboeard(gBoard)
+    // setMinesNegCount(gBoard)
+}
+
+function buildBoard() {
+    /// Build the board - starting with 4*4
+    const rowCount = 4
+    const colCount = 4
+    const board = []
+
+    for (var i = 0; i < rowCount; i++) {
+        board[i] = []
+        for (var j = 0; j < colCount; j++) {
+            const cell = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: true
+            }
+            board[i][j] = cell
+            if (!cell.isMine) MINE
+        }
+    }
+    // placing 2 MINES just for the testing 
+    board[0][2].isMine = true
+    board[2][2].isMine = true
+    console.table('board', board)
+    return board
+}
+
+/// Show on DOM
+function renderboeard(board) {
+    var strHTML = ''
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < board[0].length; j++) {
+            const currCell = board[i][j]
+            var className = 'cell'
+            if (currCell.isMine) className += 'mine'
+            if (currCell.isMarked) className += 'mark'
+            if (currCell.isShown) className += 'shown'
+
+            const title = `Cell: ${i}, ${j}`
+
+            strHTML += `<td 
+            data-i="${i}" data-j="${j}" 
+            title="${title}"  
+            class="cell ${className}" 
+            onclick="onCellClicked(this, ${i}, ${j})" ></td>`
+        }
+        strHTML += '</tr>'
+    }
+    const elBoard = document.querySelector('.board')
+    elBoard.innerHTML = strHTML
+}
+
+/// placing the count inside the cell object minesAroundCount.
+function setMinesNegCount(board) {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++)
+            if (!gBoard[i][j].isMine) {
+                gBoard[i][j].minesAroundCount = countMinesNegsAround(board, i, j)
+            }
+    }
+}
+
+
+
+/// Count mines around each cell and set the cell's minesAroundCount.
+function countMinesNegsAround(board, rowIdx, colIdx) {
+    var MinesNegsCount = 0
+    
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === board.i && j === board.j) continue
+            if (j < 0 || j >= board[0].length) continue
+            var currCell = board[i][j]
+            // console.log('currCell',currCell)
+            if (currCell.isMine) MinesNegsCount++
+        }
+    }
+    return MinesNegsCount
+}
+
+function onCellClicked(elCell, i, j) {
+    setMinesNegCount(gBoard)
+    console.log('Cell clicked: ', elCell, i, j)
+
+    const cell = gBoard[i][j]
+    console.log(cell)
+
+    if (cell.isMine) {
+        elCell.classList.add('mine')
+        elCell.innerText = MINE
+        return
+    }
+
+    if (!cell.isMine) {
+        elCell.classList.add('shown')
+        elCell.innerText = countMinesNegsAround(gBoard, i, j)
+        return
+    }
+}
+
+
+
+
